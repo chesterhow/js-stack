685c48415c7f2d97be1594eb7e1c6674c2fa7142
Chester How <chesterhow@gmail.com>
2017-06-13 18:48:07 +0800

    Switch to HashRouter
    Temporary fix to #1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 // @flow
 
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import Dev from './containers/Dev';
 import Test from './containers/Test';
 import './stylesheets/styles.scss';
 
 const App = () => (
-  <Router>
+  <Router basename="/">
     <div>
       <Route exact path="/" render={(props) => <Dev {...props} />} />
       <Route path="/testing" render={(props) => <Test {...props} />} />
